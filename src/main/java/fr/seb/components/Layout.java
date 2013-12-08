4c96de09e50a02cd33bb9442ecab8f769fc6fec8
Seb <Seb@Seb-Seven>
2013-12-08 14:44:48 +0100

    RAZ

--- src/main/java/fr/seb/components/Layout.java
+++ src/main/java/fr/seb/components/Layout.java
@@ -1,53 +1,51 @@
 package fr.seb.components;
 
-import org.apache.tapestry5.*;
-import org.apache.tapestry5.annotations.*;
-import org.apache.tapestry5.ioc.annotations.*;
 import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.Block;
+import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.ioc.annotations.Symbol;
 
 /**
  * Layout component for pages of application thermospi.
  */
-@Import(stylesheet = "context:layout/layout.css")
-public class Layout
-{
-    /**
-     * The page title, for the <title> element and the <h1> element.
-     */
-    @Property
-    @Parameter(required = true, defaultPrefix = BindingConstants.LITERAL)
-    private String title;
+@Import(stylesheet = "context:css/bootstrap.css")
+public class Layout {
+	/**
+	 * The page title, for the <title> element and the <h1>element.
+	 */
+	@Property
+	@Parameter(required = true, defaultPrefix = BindingConstants.LITERAL)
+	private String title;
 
-    @Property
-    private String pageName;
+	@Property
+	private String pageName;
 
-    @Property
-    @Parameter(defaultPrefix = BindingConstants.LITERAL)
-    private String sidebarTitle;
+	@Property
+	@Parameter(defaultPrefix = BindingConstants.LITERAL)
+	private String sidebarTitle;
 
-    @Property
-    @Parameter(defaultPrefix = BindingConstants.LITERAL)
-    private Block sidebar;
+	@Property
+	@Parameter(defaultPrefix = BindingConstants.LITERAL)
+	private Block sidebar;
 
-    @Inject
-    private ComponentResources resources;
+	@Inject
+	private ComponentResources resources;
 
-    @Property
-    @Inject
-    @Symbol(SymbolConstants.APPLICATION_VERSION)
-    private String appVersion;
+	@Property
+	@Inject
+	@Symbol(SymbolConstants.APPLICATION_VERSION)
+	private String appVersion;
 
+	public String getClassForPageName() {
+		return resources.getPageName().equalsIgnoreCase(pageName) ? "current_page_item" : null;
+	}
 
-    public String getClassForPageName()
-    {
-        return resources.getPageName().equalsIgnoreCase(pageName)
-                ? "current_page_item"
-                : null;
-    }
-
-    public String[] getPageNames()
-    {
-        return new String[]{"Index", "About", "Contact"};
-    }
+	public String[] getPageNames() {
+		return new String[] { "Index", "About", "Contact" };
+	}
 }
